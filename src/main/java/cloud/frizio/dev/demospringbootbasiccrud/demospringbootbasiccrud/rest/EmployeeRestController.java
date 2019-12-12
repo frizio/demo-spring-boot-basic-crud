abf36e993af43905b3077eb5b323124e0abd7b4c
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2019-12-12 15:03:38 +0100

    Refactor REST controller to use Service Layer

--- src/main/java/cloud/frizio/dev/demospringbootbasiccrud/demospringbootbasiccrud/rest/EmployeeRestController.java
+++ src/main/java/cloud/frizio/dev/demospringbootbasiccrud/demospringbootbasiccrud/rest/EmployeeRestController.java
@@ -2,28 +2,29 @@ package cloud.frizio.dev.demospringbootbasiccrud.demospringbootbasiccrud.rest;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import cloud.frizio.dev.demospringbootbasiccrud.demospringbootbasiccrud.dao.EmployeeDAO;
 import cloud.frizio.dev.demospringbootbasiccrud.demospringbootbasiccrud.entity.Employee;
+import cloud.frizio.dev.demospringbootbasiccrud.demospringbootbasiccrud.service.EmployeeService;
 
 @RestController
 @RequestMapping("/api")
 public class EmployeeRestController {
 
-  private EmployeeDAO employeeDAO;
+  private EmployeeService employeeService;
 
-  // Inject Employee DAO (Dirty)
-  public EmployeeRestController(EmployeeDAO employeeDAO) {
-    this.employeeDAO = employeeDAO;
+  @Autowired
+  public EmployeeRestController(EmployeeService employeeService) {
+    this.employeeService = employeeService;
   }
 
   // Expose "/employees" and return list of Employees
   @GetMapping("/employees")
   public List<Employee> findAll() {
-    return employeeDAO.findAll();
+    return employeeService.findAll();
   }
 
 }
