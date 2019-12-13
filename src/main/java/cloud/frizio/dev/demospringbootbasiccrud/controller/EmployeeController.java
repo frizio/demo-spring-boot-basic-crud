84d6799c466a993a36d7e6574c685255f4ed98eb
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2019-12-13 15:00:46 +0100

    Process form to add new employee

--- src/main/java/cloud/frizio/dev/demospringbootbasiccrud/controller/EmployeeController.java
+++ src/main/java/cloud/frizio/dev/demospringbootbasiccrud/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import cloud.frizio.dev.demospringbootbasiccrud.entity.Employee;
@@ -32,6 +34,15 @@ public class EmployeeController {
     Employee employee = new Employee();
     model.addAttribute("employee", employee);
     return "employees/employee-form";
+  }
+
+  @PostMapping("/save")
+  public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+    // Save
+    this.employeeService.save(employee);
+    // Redirect to prevent duplicate submission
+    return "redirect:/employees/list";
+
   }
 
 }
