41fdb5bba2deac9dc2d5463e7d4f7127df7dd095
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2019-12-13 13:37:20 +0100

    Get all employees in the table from db with servic

--- src/main/java/cloud/frizio/dev/demospringbootbasiccrud/controller/EmployeeController.java
+++ src/main/java/cloud/frizio/dev/demospringbootbasiccrud/controller/EmployeeController.java
@@ -1,37 +1,28 @@
 package cloud.frizio.dev.demospringbootbasiccrud.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.annotation.PostConstruct;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import cloud.frizio.dev.demospringbootbasiccrud.model.Employee;
+import cloud.frizio.dev.demospringbootbasiccrud.entity.Employee;
+import cloud.frizio.dev.demospringbootbasiccrud.service.EmployeeService;
 
 
 @Controller
+@RequestMapping("/employees")
 public class EmployeeController {
 
-  private List<Employee> employees;
-
-  // Load employee data
-  @PostConstruct
-  private void loadData() {
-    Employee employee1 = new Employee(1, "Al", "Allen", "al@email.com");
-    Employee employee2 = new Employee(2, "Bob", "Bale", "bob@email.com");
-    Employee employee3 = new Employee(3, "Carl", "Carlsonn", "carl@email.com");
-    this.employees = new ArrayList<>();
-    this.employees.add(employee1);
-    this.employees.add(employee2);
-    this.employees.add(employee3);
-  }
+  @Autowired
+  private EmployeeService employeeService;
 
   // Mapping for /list
   @GetMapping("/list")
   public String listEmployees(Model model) {
+    List<Employee> employees = this.employeeService.findAll();
     model.addAttribute("employees", employees);
     return "list-employees";
   }
