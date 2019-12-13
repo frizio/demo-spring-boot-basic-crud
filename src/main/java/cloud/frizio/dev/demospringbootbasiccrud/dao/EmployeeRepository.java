d8e1f9b67112f66646795ce3fc2db242770262a6
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2019-12-13 09:57:17 +0100

    Spting Data Rest: Configuration Pagination Sorting

--- src/main/java/cloud/frizio/dev/demospringbootbasiccrud/dao/EmployeeRepository.java
+++ src/main/java/cloud/frizio/dev/demospringbootbasiccrud/dao/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package cloud.frizio.dev.demospringbootbasiccrud.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import cloud.frizio.dev.demospringbootbasiccrud.entity.Employee;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
   // that's it for simple CRUD methods!!!!
